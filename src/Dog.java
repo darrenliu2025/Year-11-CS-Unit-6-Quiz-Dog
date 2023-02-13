d40c49f9c7385df930d60b2a47644e0f7db68861
darrenliu <darrenl2025@student.cis.edu.hk>
2023-02-13 12:48:59 +0800

    Added dog class

--- src/Dog.java
+++ src/Dog.java
@@ -1,2 +1,41 @@
 public class Dog {
+    private int age;
+    private String name;
+    private String color;
 }
+    public Dog() {
+        this.age = 0;
+        this.name = "";
+        this.color = "";
+       }
+    public Dog(int age, String name, String color) {
+        this.age = age;
+        this.name = name;
+        this.color = color;
+    }
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setAge(age){
+        this.age = age;
+    }
+    public void setName(name){
+        this.name = name;
+    }
+    public void setColor(color){
+        this.color = color;
+    }
+    private void addOneAge(){
+        age = age + 1;
+    }
+    private int birthday(){
+        public void addOneAge();
+        return age;
+    }
+
--- src/DogMaker.java
+++ src/DogMaker.java
@@ -1,8 +1,6 @@
 public class DogMaker {
-  
   public static void main(String[] args) {
-    
-    
-  }
-  
+    Dog dog = new Dog(2, "Jill", "Black and tan");
+    dog.setName("Jessie"");
+    System.out.println(dog.getname());
 }
